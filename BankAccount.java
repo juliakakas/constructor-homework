ab7f26e8d511ec63e88a5de234ffa01724b3445e
Lia <julia.kakas@gmail.com>
2023-06-09 20:43:35 +0200

    Házit írtam

--- BankAccount.java
+++ BankAccount.java
@@ -1,3 +1,48 @@
 public class BankAccount {
-    // TODO
+    private String accountNumber;
+    private String accountHolder;
+    private double balance;
+
+    public BankAccount(String accountNumber,String accountHolder, double balance){
+        this.accountNumber = accountNumber;
+        this.accountHolder = accountHolder;
+        this.balance = balance;
+        System.out.println("My little constructor <3");
+    }
+    public BankAccount(){
+        this("Unknown", "Unknown", 1000000);
+    }
+
+    public BankAccount(String accountNumber){
+        this.accountNumber = accountNumber;
+    }
+
+    public BankAccount(double balance){
+        this.balance = balance;
+    }
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getAccountHolder() {
+        return accountHolder;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public void setAccountHolder(String accountHolder) {
+        this.accountHolder = accountHolder;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+
 }
--- Main.java
+++ Main.java
@@ -5,5 +5,19 @@ public class Main {
 
         Car defaultCar = new Car();
         System.out.println(defaultCar.getModel()); // Output: Unknown
+
+        BankAccount myAccount = new BankAccount("1778322544", "Kakas Júlia", 1506.5);
+        System.out.println(myAccount.getBalance());
+
+        BankAccount revolutAccount = new BankAccount("7889234566", "Júlia Kakas", 312.3);
+        System.out.println(revolutAccount.getBalance());
+
+        BankAccount account = new BankAccount("666");
+        System.out.println(account.getAccountNumber());
+        BankAccount newBalance = new BankAccount(456.8);
+        System.out.println(newBalance.getBalance());
+
+        BankAccount defaultAccount = new BankAccount();
+        System.out.println(defaultAccount.getAccountHolder());
     }
 }
